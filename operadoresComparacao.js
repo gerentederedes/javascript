32688fbc9b87958f1f8cc770e344d2f437908f3d
Patrick Luiz <gerentederedes@gmail.com>
2025-11-08 23:07:51 -0300

    08112025

---
+++ operadoresComparacao.js
@@ -0,0 +1,14 @@
+const idadeVinny = 25;
+const idadeMaria = 30;
+
+const saoIguais = idadeVinny === idadeMaria;        
+console.log('São iguais?', saoIguais);
+
+const vinnyEhMaisVelho = idadeVinny > idadeMaria;  
+console.log('Vinny é mais velho que Maria?', vinnyEhMaisVelho);
+
+const souMaiorDeIdade = idadeVinny >= 18;
+console.log('Vinny é maior de idade?', souMaiorDeIdade);    
+
+const mariaEhMenor = idadeMaria < 18;
+console.log('Maria é menor de idade?', mariaEhMenor);   
---
+++ operadoresMatematicos.js
@@ -0,0 +1,51 @@
+const numeroUm = 4
+const numeroDois = 8
+
+const subtracao = numeroDois - numeroUm
+console.log('Resultado da subtração: ' + subtracao)
+
+const multiplicacao = numeroUm * numeroDois
+console.log('Resultado da multiplicação: ' + multiplicacao) 
+
+const divisao = numeroDois / numeroUm
+console.log('Resultado da divisão: ' + divisao) 
+
+const soma = numeroUm + numeroDois
+console.log('Resultado da soma: ' + soma)   
+
+const restoDaDivisao = numeroDois % numeroUm
+console.log('Resto da divisão: ' + restoDaDivisao)  
+
+let incremento = numeroUm
+incremento++
+console.log('Valor após incremento: ' + incremento) 
+
+let decremento = numeroDois
+decremento--
+console.log('Valor após decremento: ' + decremento)     
+
+// Operadores combinados
+
+let contador = 10
+contador += 5 // contador = contador + 5
+console.log('Contador após += 5: ' + contador)      
+contador -= 3 // contador = contador - 3
+console.log('Contador após -= 3: ' + contador)      
+contador *= 2 // contador = contador * 2                
+
+console.log('Contador após *= 2: ' + contador)      
+contador /= 4 // contador = contador / 4                
+console.log('Contador após /= 4: ' + contador)  
+console.log('Contador após %= 4: ' + contador)      
+contador %= 4 // contador = contador % 4    
+
+console.log('Contador após %= 4: ' + contador)
+
+let potencia = 2 ** 3
+console.log('Resultado da potenciação: ' + potencia)    
+
+const contaCompleta = ((numeroUm + numeroDois) * 2) / 4
+console.log('Resultado da conta completa: ' + contaCompleta)        
+      
+
+
